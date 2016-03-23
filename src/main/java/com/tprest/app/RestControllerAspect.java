067d9470e35a79cc549745f23e405b1f222594b8
marwensaid <marwensaidi1@gmail.com>
2016-03-23 11:05:37 +0100

    feat(SB-Rest-App)  Rest controller Def--MS

---
+++ src/main/java/com/tprest/app/RestControllerAspect.java
@@ -0,0 +1,21 @@
+package com.tprest.app;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by marwen on 23/03/16.
+ */
+
+@Aspect
+@Component
+public class RestControllerAspect {
+
+    @Before("execution(public * com.khoubyari.example.api.rest.*Controller.*(..))")
+    public void logBeforeRestCall(JoinPoint pjp) throws Throwable {
+        System.out.println(":::::AOP Before REST call:::::" + pjp);
+    }
+
+}
